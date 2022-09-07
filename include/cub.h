3ce402bb746eb78735796e10fef4e7ad08776e6a
Ajjig <majjig@pm.me>
2022-09-07 15:02:35 +0100

    :heavy_plus_sign: add t_sprite t_game structs

--- include/cub.h
+++ include/cub.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 18:33:40 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/09/07 12:10:47 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/09/07 14:55:22 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,27 @@
 #include <fcntl.h>
 #include <mlx.h>
 
-void    error_handler(char *message);
+
+typedef struct s_sprite{
+	int		x;
+	int		y;
+	int		texture;
+	int		distance;
+}				t_sprite;
+
+typedef struct s_game {
+	char		**map;
+	int			map_height;
+	int			map_width;
+	char		*north;
+	char		*south;
+	char		*west;
+	char		*east;
+	t_sprite	*sprite;
+	uint		color_floor;
+	uint		color_ceiling;
+}				t_game;
+
+void	error_handler(char *message);
 
 #endif
