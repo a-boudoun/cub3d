f40f894ceae798995b12455c4d702843041d24a4
Ajjig <majjig@pm.me>
2022-09-12 15:24:30 +0100

    Added dynamic draw player position

--- src/drawing/drawing_map.c
+++ src/drawing/drawing_map.c
@@ -18,6 +18,24 @@ void drw_box(t_data *data, int x_b, int y_b, int color)
 	}
 }
 
+void drw_player(t_data *data, int x_b, int y_b, int color)
+{
+	int x;
+	int y;
+
+	y = 0;
+	while (y < BOX_Y / 2)
+	{
+		x = 0;
+		while (x < BOX_X / 2)
+		{
+			mlx_pixel_put(data->mlx, data->win, x_b + x, y_b + y, color);
+			x++;
+		}
+		y++;
+	}
+}
+
 void	draw_map(t_data *data)
 {
 	int x;
@@ -31,12 +49,11 @@ void	draw_map(t_data *data)
 		{
 			if (data->game->map[y][x] == '1')
 				drw_box(data, x * BOX_X, y * BOX_Y, 0xFFFFFF);
-			else if (data->game->map[y][x] == '0')
+			else if (data->game->map[y][x] == EMPTY || data->game->map[y][x] == INIT_POS)
 				drw_box(data, x * BOX_X, y * BOX_Y, data->game->color_floor);
-			else if (data->game->map[y][x] == 'N')
-				drw_box(data, x * BOX_X, y * BOX_Y, 0x778899);
 			x++;
 		}
 		y++;
 	}
+	drw_player(data, data->player->x * BOX_X, data->player->y * BOX_Y, 0xFF0000);
 }
