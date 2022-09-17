c140095233fd4a001925701f806ef53cc6a67e25
Ajjig <majjig@pm.me>
2022-09-17 17:30:59 +0100

    :bug: Fix minimap length issue

--- src/main.c
+++ src/main.c
@@ -65,8 +65,7 @@ int main(int ac, char **av)
 	get_player_pos(&data);
 	data.img_game->img = mlx_new_image(data.mlx, WIN_WIDTH, WIN_HEIGHT);
 	data.img_game->addr = mlx_get_data_addr(data.img_game->img, &data.img_game->bits_per_pixel, &data.img_game->line_length, &data.img_game->endian);
-	data.img->img = mlx_new_image(data.mlx, (WIN_WIDTH / 4 - (WIN_WIDTH / 4-(data.minimap->box_width*(data.game->map_width)))),
-		(WIN_HEIGHT / 4 - (WIN_HEIGHT/4 - data.game->map_height * data.minimap->box_height)));
+	data.img->img = mlx_new_image(data.mlx, (data.minimap->box_width*(data.game->map_width)), data.game->map_height * data.minimap->box_height);
 	data.img->addr = mlx_get_data_addr(data.img->img, &data.img->bits_per_pixel, \
 		&data.img->line_length, &data.img->endian);
 	data.player->angle = 60 * PI / 180;
