7790f75c5011f531626d7e7291240d4ad55bf509
Ajjig <majjig@pm.me>
2022-09-25 16:56:21 +0100

    tkhrbi9

--- src/utils/pixels.c
+++ src/utils/pixels.c
@@ -3,13 +3,9 @@
 unsigned int get_pixel(t_img *img, int x, int y, double wall_height)
 {
 	unsigned int color;
-	img += 0;
-	x += 0;
-	y += 0;
 
 
-	// the brightness of the color is proportional to the wall_height / WIN_HEIGHT from the player
-	int brightness = 0xFF -(int)(0xFF * (1 - wall_height / WIN_HEIGHT));
-	color = (brightness << 16) | (brightness << 8) | brightness;
+	color = *(unsigned int *)(img->addr + ((y % img->height) * img->line_length + (x % img->width) * (img->bits_per_pixel / 8)));
+
 	return (color);
 }
