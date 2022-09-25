1424e86983d1d812e3d2d39056a8965ced5356a4
abderrahim <abderrahimboudounit@gmail.com>
2022-09-25 19:16:01 +0100

    counting with 64 and and drawing with 6

--- src/drawing/draw_game.c
+++ src/drawing/draw_game.c
@@ -2,28 +2,33 @@
 
 
 
-void	drw_column(t_data *data, int y, double dist)
+void	drw_column(t_data *data, int x, int dist, bool is_horizontal)
 {
 	// draw line from top to bottom aligned to center of WIN_HEIGHT
-	int x = 0;
 
-	while (x < WIN_HEIGHT /2)
+	int offsetX;
+	int y = 0;
+	if (is_horizontal)
+		offsetX = (int)data->rays_x[x] % BOX_SIZE;
+	else
+		offsetX = (int)data->rays_y[x] % BOX_SIZE;
+	while (y < WIN_HEIGHT / 2)
 	{
-		my_mlx_pixel_put(data->img_game, y, x, data->game->color_ceiling);
-		x++;
+		my_mlx_pixel_put(data->img_game, x, y, data->game->color_ceiling);
+		y++;
 	}
-	x = WIN_HEIGHT - 1;
-	while (x > WIN_HEIGHT / 2)
+	y = WIN_HEIGHT - 1;
+	while (y > WIN_HEIGHT / 2)
 	{
-		my_mlx_pixel_put(data->img_game, y, x, data->game->color_floor);
-		x--;
+		my_mlx_pixel_put(data->img_game, x, y, data->game->color_floor);
+		y--;
 	}
 	//printf("floor_color: %d\n", data->game->color_floor);
-	x = ((WIN_HEIGHT - dist) / 2);
-	while (x <  (WIN_HEIGHT + ((int)(dist + .5))) / 2)
+	y = ((WIN_HEIGHT - dist) / 2);
+	while (y <  (WIN_HEIGHT + ((int)(dist + .5))) / 2)
 	{
-		my_mlx_pixel_put(data->img_game, y, x, get_pixel(data->sprite->north, x, y, dist));
-		x++;
+		my_mlx_pixel_put(data->img_game, x, y, get_pixel(data->sprite->north, offsetX, y, dist));
+		y++;
 	}
 }
 
@@ -33,7 +38,7 @@ void	draw_game(t_data *data)
 	int i = 899;
 	int col = 0;
 	data->img_game->img = mlx_new_image(data->mlx, WIN_WIDTH, WIN_HEIGHT);
-	data->img_game->addr = mlx_get_data_addr(data->img_game->img, &data->img_game->bits_per_pixel, &data->img_game->line_length, &data->img_game->endian);
+	data->img_game->addr = (int *) mlx_get_data_addr(data->img_game->img, &data->img_game->bits_per_pixel, &data->img_game->line_length, &data->img_game->endian);
 	while(i >= 0)
 	{
 		int dist = WIN_HEIGHT *  BOX_SIZE / (data->rays_dist[i] + 0.0001);
@@ -42,7 +47,7 @@ void	draw_game(t_data *data)
 		if (dist <= 0)
 			dist = 0;
 		//unsigned transparency = (dist * 0xFF / (WIN_HEIGHT));
-		drw_column(data, col++, dist);
+		drw_column(data, col++, dist, data->is_horizontal[i]);
 		i--;
 	}
 	mlx_put_image_to_window(data->mlx, data->win, data->img_game->img, 0, 0);
--- src/utils/pixels.c
+++ src/utils/pixels.c
@@ -3,9 +3,12 @@
 unsigned int get_pixel(t_img *img, int x, int y, double wall_height)
 {
 	unsigned int color;
+	(void)wall_height;
 
+	y = y - ((WIN_HEIGHT - wall_height) / 2);
+	y *= 64 /  wall_height;
 
-	color = *(unsigned int *)(img->addr + ((y % img->height) * img->line_length + (x % img->width) * (img->bits_per_pixel / 8)));
+	color = *(unsigned int *)(img->addr + (y * (img->line_length / 4) + x));
 
 	return (color);
 }
