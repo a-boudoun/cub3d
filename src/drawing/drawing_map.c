1ed332d75c6c4a744fccf8f3a1cfb1f4b05edb71
abderrahim <abderrahimboudounit@gmail.com>
2022-09-26 20:19:51 +0100

    fixing texture rendring issue

--- src/drawing/drawing_map.c
+++ src/drawing/drawing_map.c
@@ -4,6 +4,8 @@ void	my_mlx_pixel_put(t_img *data, int x, int y, unsigned int color)
 {
 	char	*dst;
 
+	if (x < 0 || x >= WIN_WIDTH || y < 0 || y >= WIN_HEIGHT)
+		return ;
 	dst = (char *)data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
