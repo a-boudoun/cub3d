7d34d9f2b2b69446a634699b9b9131dc4c6f944f
Ajjig <majjig@pm.me>
2022-09-28 16:55:47 +0100

    NORMINATED

--- src/parsing/check_path.c
+++ src/parsing/check_path.c
@@ -6,10 +6,9 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 12:21:02 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/09/28 15:10:51 by majjig           ###   ########.fr       */
+/*   Updated: 2022/09/28 16:55:30 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "cub.h"
 
@@ -21,6 +20,18 @@ static int	is_valid(char *path)
 	if (fd < 0)
 		error_handler("Invalid texture");
 	return (fd);
+}
+
+void	load_xpm(t_data *data, t_img *img, char *path)
+{
+	img->img = mlx_xpm_file_to_image(data->mlx, path,
+			&(img->width), &(img->height));
+	if (img->img == NULL)
+		error_handler("Invalid texture");
+	img->addr = (int *) mlx_get_data_addr(img->img, &(img->bits_per_pixel),
+			&(img->line_length), &(img->endian));
+	if (img->addr == NULL)
+		error_handler("Invalid texture");
 }
 
 t_sprite	*check_elements_path(t_data *data)
@@ -38,27 +49,13 @@ t_sprite	*check_elements_path(t_data *data)
 	east = (t_img *) malloc(sizeof(t_img));
 	ft_bzero(sprite, sizeof(t_sprite));
 	if (is_valid(data->game->north))
-	{
-		north->img = mlx_xpm_file_to_image(data->mlx, data->game->north, &(north->width), &(north->height));
-		north->addr = (int*)mlx_get_data_addr(north->img, &(north->bits_per_pixel), &(north->line_length), &(north->endian));
-	}
+		load_xpm(data, north, data->game->north);
 	if (is_valid(data->game->south))
-	{
-		south->img = mlx_xpm_file_to_image(data->mlx, data->game->south, &(south->width), &(south->height));
-		south->addr = (int*)mlx_get_data_addr(south->img, &(south->bits_per_pixel), &(south->line_length), &(south->endian));
-	}
+		load_xpm(data, south, data->game->south);
 	if (is_valid(data->game->west))
-	{
-		west->img = mlx_xpm_file_to_image(data->mlx, data->game->west, &(west->width), &(west->height));
-		if (west->img == NULL)
-			error_handler("Invalid texture");
-		west->addr = (int*)mlx_get_data_addr(west->img, &(west->bits_per_pixel), &(west->line_length), &(west->endian));
-	}
+		load_xpm(data, west, data->game->west);
 	if (is_valid(data->game->east))
-	{
-		east->img = mlx_xpm_file_to_image(data->mlx, data->game->east, &(east->width), &(east->height));
-		east->addr = (int*)mlx_get_data_addr(east->img, &(east->bits_per_pixel), &(east->line_length), &(east->endian));
-	}
+		load_xpm(data, east, data->game->east);
 	sprite->north = north;
 	sprite->south = south;
 	sprite->west = west;
