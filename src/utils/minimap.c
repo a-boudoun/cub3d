8beb0de543637909e915a28cb2d959d28d0c868f
Ajjig <majjig@pm.me>
2022-10-06 15:33:57 +0100

    NORMINATED

--- src/utils/minimap.c
+++ src/utils/minimap.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 15:30:26 by majjig            #+#    #+#             */
-/*   Updated: 2022/10/06 15:30:27 by majjig           ###   ########.fr       */
+/*   Updated: 2022/10/06 15:33:13 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ void	circlular_minimap(int *img)
 {
 	int	x;
 	int	y;
+	int	radius;
 
+	radius = MINI / 2 * 6;
 	y = 0;
 	while (y < MINI * 6)
 	{
 		x = 0;
 		while (x < MINI * 6)
 		{
-			if ((hypot(x - MINI / 2 * 6, y - MINI / 2 * 6) > MINI / 2 * 6))
+			if ((hypot(x - radius, y - radius) > radius))
 				img[y * MINI * 6 + x] = 0xFF000000;
-			else if ((hypot(x - MINI / 2 * 6, y - MINI / 2 * 6) >= MINI / 2 * 6 - 1))
+			else if ((hypot(x - radius, y - radius) >= radius - 1))
 				img[y * MINI * 6 + x] = 0xFFFFFF;
 			x++;
 		}
